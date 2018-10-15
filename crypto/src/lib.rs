15b5dfba1cb554501a1be960c922be7acd9dba20
xiaohuo <xiaohuo200@gmail.com>
2018-10-16 02:03:44 +0800

    develop sha256 fn

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,7 +1,30 @@
+extern crate basictype;
+extern crate crypto;
+
+use basictype::hash::Hash256;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+
+
+pub fn sha256(input: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.input_str(input);
+
+//    let out: &mut [u8] = &mut []; //都是泪啊
+    let out = hasher.result_str();
+    out
+}
+
+
 #[cfg(test)]
 mod tests {
+    use sha256;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_sha256() {
+        let out = sha256("hello");
+        let expected = String::from("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
+        println!("{}", out);
+        assert_eq!(out, expected)
     }
 }
