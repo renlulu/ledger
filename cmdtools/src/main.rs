3ccc17f9e031257929381d68f8aa7d86bc9d5fd3
xiaohuo <xiaohuo200@gmail.com>
2018-10-16 04:43:43 +0800

    fix args

--- cmdtools/src/main.rs
+++ cmdtools/src/main.rs
@@ -1,13 +1,17 @@
 extern crate crypto;
 
 use crypto::sha256;
+use crypto::double_sha256;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match &args[1][..] {
         "sha256" => {
-            println!("{}", sha256(&args[1]));
+            println!("{}", sha256(&args[2]));
+        }
+        "double_sha256" => {
+            println!("{}",double_sha256(&args[2]));
         }
         _ => {
             println!("{:?}", &args)
